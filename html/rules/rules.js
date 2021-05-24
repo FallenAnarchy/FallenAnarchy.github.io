a02bf7d5ba76d1f248791b6de406bfc87d355615
FallenAnarchy <84747618+FallenAnarchy@users.noreply.github.com>
2021-05-24 22:54:50 +0100

    Update rules.js

--- html/rules/rules.js
+++ html/rules/rules.js
@@ -12,3 +12,9 @@ function fa() {
   console.log('fva')
 
 }
+function darkmode() {
+  var style = document.body.style
+  style.backgroundColor = style.backgroundColor === "white" ? "black" : "white"
+  document.getElementById('mode').innerHTML = style.backgroundColor === "white" ? "Dark mode" : "Light mode"
+    document.getElementById('mode').style.color = style.backgroundColor === "white" ? "Black" : "White"
+}
